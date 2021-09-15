f72493b3ebbcf2253a5cdbcab146ccedc7b2b3b6
aabazary <aabazary@gmail.com>
2021-09-15 00:32:51 -0700

    working on routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const {
+  User,
+  
+} = require('../models');
+// const withAuth = require('../utils/auth');
+
+
+router.get('/', async (req, res) => {
+    res.render('dashboard');    
+});
+
+module.exports = router;
