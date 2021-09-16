3208b03c595feded075182c48a3f2a9445dc2134
aabazary <aabazary@gmail.com>
2021-09-16 11:58:48 -0700

    working on login dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,13 +1,46 @@
 const router = require('express').Router();
 const {
   User,
+  Post,
+  Comment
   
 } = require('../models');
 // const withAuth = require('../utils/auth');
 
 
 router.get('/', async (req, res) => {
-    res.render('dashboard');    
+  Post.findAll({
+    where: {
+        user_id: req.session.user_id
+    },
+    attributes: [
+        'id',
+        'title',
+        'content',
+        'createdAt'
+    ],
+    include: [{
+            model: Comment,
+            attributes: ['id', 'post_id', 'user_id', ],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+    ]
+})
+.then(postDB => {
+    const posts = postDB.map(post => post.get({ plain: true }));
+    res.render('dashboard', { posts, loggedIn: true });
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});  
 });
 
 module.exports = router;
