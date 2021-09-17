42ef6cb759eb931d0da475137cc6626de370e8a0
aabazary <aabazary@gmail.com>
2021-09-17 16:50:31 -0700

    updating ui, working on comments

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -94,6 +94,80 @@ router.get('/edit/:id', (req, res) => {
         });
 })
 
+router.get('/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'title',
+                'content',
+                'createdAt'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'post_id', 'user_id', 'createdAt'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
 
+        .then(postData => res.json(postData))
+        
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+router.put('/:id', (req, res) => {
+    Post.update({
+            title: req.body.title,
+            content: req.body.content
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(postData => {
+            if (!postData) {
+                res.status(404).json({
+                    message: 'No post found'
+                });
+                return;
+            }
+            res.json(postData);
+            console.log(postData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(postData => {
+        if (!postData) {
+            res.status(404).json({
+                message: 'No post found'
+            });
+            return;
+        }
+        res.json(postData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
